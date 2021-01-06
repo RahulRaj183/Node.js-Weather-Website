d25fc9bd8b29e05ca14b13973f72e95565ff0fc1
RAHUL RAJ <rahulraj.nits.ece@gmail.com>
2021-01-06 10:48:03 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('', (req,res) => {
 
 app.get('/about', (req,res) =>{
      res.render('about', {  
-        title:'About Page',
+        title:'About Me',
         name: 'Rahul Raj'
        })
 })
